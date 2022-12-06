5193c4283e7d7664c8c740dc3c4ec699c51d6e51
Muhammad Salman <87569242+Salman-D@users.noreply.github.com>
2022-12-06 21:15:25 +0500

    chore(Title Changed)

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Step_form from './Components/Stepper';
 function App() {
   return (
     <div >
-      <h1 className='text-center mt-3 text-dark fw-bold'>Saylani  Mass IT Program</h1>
-      <h2 className='text-center h4 text-uppercase text-black-50 mb-3'>Registration Form</h2>
+      <h1 className='text-center mt-3 text-dark fw-bold'>User Registration Form</h1>
+      <h5 className='text-center h4 text-uppercase text-black-50 mb-3'>develop by Muhammad Salman</h5>
       <Step_form/>
     </div>
   );
